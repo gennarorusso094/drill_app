241668ca165c0a5c9381c06ec4d612f8dd59969c
gennarorusso094 <121187789+gennarorusso094@users.noreply.github.com>
2022-12-22 13:55:28 +0100

    Feature/es 1 (#2)
    * ADDED completed feature nr.1

---
+++ src/CONSTANTS.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+
+//API-s URLs
+export const REGIONS_URL =
+    "https://apiv3.iucnredlist.org/api/v3/region/list?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee";
---
+++ src/pages/RegionsPage.tsx
@@ -0,0 +1,29 @@
+import React, { useCallback, useEffect } from "react";
+
+import { REGIONS_URL } from "../CONSTANTS";
+
+export const RegionsPage: React.FC = () => {
+  const fetchRegions = useCallback(async () => {
+    try {
+      const response = await fetch(REGIONS_URL);
+
+      if (!response.ok) {
+        throw new Error("Error occurred when fetching regions!");
+      }
+
+      const data = await response.json();
+
+      console.log(data.results); // TODO: remove this console log in next tasks
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      alert(error.message);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchRegions();
+  }, [fetchRegions]);
+
+  return null;
+};
