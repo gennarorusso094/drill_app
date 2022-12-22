7355d9f8e733937905c94e52f94e1fdd6ddaeae5
gennaro.russo <gennaro.russo@kineton.it>
2022-12-22 16:46:00 +0100

    ADDED species Model

--- src/models.ts
+++ src/models.ts
@@ -2,3 +2,11 @@ export interface Region {
   identifier: string;
   name: string;
 }
+
+export interface Species {
+  class: string;
+  family: string;
+  kingdomName: string;
+  mainCommonName: string;
+  scientificName: string;
+}
